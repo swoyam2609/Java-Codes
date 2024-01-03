72efea9c82603dad2fdc96fc0b0def9123bac1cf
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2024-01-03 16:52:40 +0530

    skipping advanced patterns

---
+++ ApnaCollegeCourse/L06AdvancedPatterns/pattern0.java
@@ -0,0 +1,12 @@
+package ApnaCollegeCourse.L06AdvancedPatterns;
+
+import java.util.*;
+
+public class pattern0 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of columns: ");
+        int a = sc.nextInt();
+        
+    }
+}
